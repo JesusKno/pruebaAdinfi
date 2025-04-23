38e1250269fa3ff22fddd346e75d5a84cc4afd90
JesusKno <57113540+JesusKno@users.noreply.github.com>
2025-04-23 00:28:01 -0600

    avance al momento

--- numero_letra.py
+++ numero_letra.py
@@ -2,12 +2,28 @@ import string
 
 
 def numero_letra(numero: int):
-    if(numero < 0):
+    if(numero < 1):
         print("Por favor de introducir un numero valido")
     
-    letra = string.ascii_letters[numero]
+    letra_numero_calcular = numero
+    contador = 0
     
-    return print(letra)
+    resultado = []
+    
+    
+    while letra_numero_calcular > 0:
+        letra_numero_calcular = (letra_numero_calcular - 1) // 26
+        contador += 1
+    
+    numero2 = numero
+    
+    
+    for _ in range(contador):
+        
+        numero2, restante = divmod(numero2 - 1, 26)
+        resultado.append(string.ascii_uppercase[restante])
+    
+    return print("".join(resultado))
 
 
-numero_letra(1)
+numero_letra(28)
